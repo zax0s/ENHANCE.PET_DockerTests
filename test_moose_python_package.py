637ea2920e48458e1613e7d599a24db87bbb6218
Zacharias Chalampalakis <zacharias.chalampalakis@meduniwien.ac.at>
2023-10-13 14:06:49 +0200

    orca bug with moosez recreation

---
+++ test_moose_python_package.py
@@ -0,0 +1,3 @@
+from moosez import moose
+
+moose("clin_ct_body", "orca_test_directory_bug/ORCA-V01-2023-10-13-11-43-07/CT/resampled_toPET_CT_img", "orca_test_directory_bug/ORCA-V01-2023-10-13-11-43-07/masks", "cuda")
